d8c6a661e520dc7739460d692ae5c254fcf219b2
test <test>
2023-11-11 16:15:05 +0700

    test

---
+++ coffee.py
@@ -0,0 +1,13 @@
+amount_due=50
+while amount_due > 0:
+
+    print("Нужная сумма:", amount_due)
+
+    coin = int(input ("Вставьте монету: "))
+
+    if coin in [50, 20, 10, 5]:
+
+        amount_due -= coin
+change_owed=abs(amount_due)
+
+print ("Ваша сдача:", change_owed)
